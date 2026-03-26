2f754b761fe4f953c10a5d8690f084d697207375
x-shakuni <shaakuni@outlook.in>
2026-03-26 22:06:51 +0530

    Add files via upload

---
+++ scripts/bronze/bronze_data_defination.sql
@@ -0,0 +1,74 @@
+------------------------------------------------
+---------------TABLE DEFINATION-----------------
+------------------------------------------------
+
+
+IF OBJECT_ID ('bronze.crm_cust_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_material_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('bronze.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_name NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_date DATETIME,
+	prd_end_date DATETIME
+);
+
+-- sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price
+
+IF OBJECT_ID ('bronze.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+
+);
+
+--CID,BDATE,GEN
+IF OBJECT_ID ('bronze.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gender NVARCHAR(50)
+);
+
+--CID,CNTRY
+
+IF OBJECT_ID ('bronze.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101(
+	cid NVARCHAR(50),
+	country NVARCHAR(50)
+);
+
+--AC_CL,Accessories,Cleaners,Yes
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+
